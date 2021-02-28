e7b15f6f3d5f40e032143d53a91b5533ab271a50
Sergey Senozhatsky <senozhatsky@chromium.org>
2021-02-28 16:02:57 +0900

    videobuf2: add V4L2_FLAG_MEMORY_NON_COHERENT flag
    By setting or clearing V4L2_FLAG_MEMORY_NON_COHERENT flag
    user-space should be able to hint vb2 that either a non-coherent
    (if supported) or coherent memory should be used for the buffer
    allocation.
    
    The patch set also adds a corresponding capability flag:
    fill_buf_caps() reports V4L2_BUF_CAP_SUPPORTS_MMAP_CACHE_HINTS
    when queue supports user-space cache management hints.
    
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -954,6 +954,8 @@ struct v4l2_requestbuffers {
 	__u32			reserved[1];
 };
 
+#define V4L2_FLAG_MEMORY_NON_COHERENT			(1 << 0)
+
 /* capabilities for struct v4l2_requestbuffers and v4l2_create_buffers */
 #define V4L2_BUF_CAP_SUPPORTS_MMAP			(1 << 0)
 #define V4L2_BUF_CAP_SUPPORTS_USERPTR			(1 << 1)
